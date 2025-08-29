6394145f9390c7f9132e4d69eef5fed682adce00
Vivek000777 <149562421+Vivek000777@users.noreply.github.com>
2025-08-29 15:06:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.get('/todo',(req,res)=>{
     res.json({
-        message : 'my name is Lumax and I work work whole night and rest whole day'
+        message : 'my name is Lumax and I work  whole night and rest whole day'
     })
 })
 
@@ -12,5 +12,6 @@ app.listen(3000,()=>{
     console.log('listening');
     
 })
+
 
 
